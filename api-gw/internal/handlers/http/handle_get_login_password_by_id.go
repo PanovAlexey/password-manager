ec5f00062d4c120bab5758033bbf7c0d9a9889b3
Panov Alexey <alexeykapanov@gmail.com>
2022-07-23 21:37:21 +0300

    Create handler for getting login password by id.

---
+++ api-gw/internal/handlers/http/handle_get_login_password_by_id.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	pb "api-gw/pkg/user_data_manager_grpc"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func (h *httpHandler) HandleGetLoginPasswordById(w http.ResponseWriter, r *http.Request) {
+	userId := fmt.Sprintf("%v", r.Context().Value("token"))
+	id := chi.URLParam(r, "id")
+
+	response, err := (*h.gRPCUserDataManagerClient.GetClient()).GetLoginPasswordById(
+		context.Background(), &pb.GetLoginPasswordByIdRequest{
+			Id:     id,
+			UserId: userId,
+		},
+	)
+
+	if err != nil {
+		h.logger.Error("error getting login-password by id: "+err.Error(), id, userId)
+	}
+
+	h.logger.Info("successful getting login-password by id successful ", id, userId, response.LoginPassword.Name)
+	result, err := json.Marshal(response)
+
+	if err != nil {
+		h.logger.Error("error marshalling login-password: "+err.Error(), id, userId)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
