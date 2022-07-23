b78622a2ac48c043952fefe012f4651714bcf93d
Panov Alexey <alexeykapanov@gmail.com>
2022-07-23 21:37:14 +0300

    Create handler for getting login password list.

---
+++ api-gw/internal/handlers/http/handle_get_login_password_list.go
@@ -0,0 +1,32 @@
+package http
+
+import (
+	pb "api-gw/pkg/user_data_manager_grpc"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func (h *httpHandler) HandleGetLoginPasswordList(w http.ResponseWriter, r *http.Request) {
+	userId := fmt.Sprintf("%v", r.Context().Value("token"))
+
+	response, err := (*h.gRPCUserDataManagerClient.GetClient()).GetLoginPasswordList(
+		context.Background(),
+		&pb.GetLoginPasswordListRequest{UserId: userId},
+	)
+
+	if err != nil {
+		h.logger.Error("error getting login-password list: "+err.Error(), userId)
+	}
+
+	h.logger.Info("successful getting login-password list by user id ", userId, response)
+	result, err := json.Marshal(response)
+
+	if err != nil {
+		h.logger.Error("error marshalling login-password list: "+err.Error(), userId, response)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
