b27522caab8b769e8de0ba82a6de91425103a5f0
Panov Alexey <alexeykapanov@gmail.com>
2022-07-26 17:50:20 +0300

    Use custom config for logger.

--- api-gw/internal/infrastructure/logging/logger.go
+++ api-gw/internal/infrastructure/logging/logger.go
@@ -2,17 +2,36 @@ package logging
 
 import (
 	"api-gw/internal/config"
+	"encoding/json"
 	"go.uber.org/zap"
 	"log"
 )
 
 func GetLogger(config config.Config) *zap.SugaredLogger {
-	zapLogger, err := zap.NewProduction()
-	zapLogger = zapLogger.Named(config.GetApplicationName())
+	rawJSON := []byte(`{
+   "level": "debug",
+   "encoding": "json",
+   "outputPaths": ["stdout"],
+   "errorOutputPaths": ["stderr"],
+   "encoderConfig": {
+     "messageKey": "message",
+     "levelKey": "level",
+     "levelEncoder": "lowercase"
+   }
+ }`)
+	var cfg zap.Config
+
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+
+	zapLogger, err := cfg.Build()
 
 	if err != nil {
 		log.Fatalf("can't initialize zap logger: %v", err)
 	}
+
+	zapLogger = zapLogger.Named(config.GetApplicationName())
 
 	return zapLogger.Sugar()
 }
