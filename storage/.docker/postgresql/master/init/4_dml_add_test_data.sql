7b7654ea4138206c4616d2ece39eb45c747f8f98
Panov Alexey <alexeykapanov@gmail.com>
2022-08-03 14:32:43 +0300

    Fix migration for test data creation.

--- storage/.docker/postgresql/master/init/4_dml_add_test_data.sql
+++ storage/.docker/postgresql/master/init/4_dml_add_test_data.sql
@@ -1,4 +1,4 @@
 insert into users
-    (id, email, password, created_at)
+    (id, email, password, created_at, last_access_at)
 values
-    (1, 'test@test.org', 'qwertyuioplkjhgfdszxxcvbnm', '2022-09-01 20:00:47');
+    (1, 'test@test.org', 'qwertyuioplkjhgfdszxxcvbnm', '2022-09-01 20:00:47', '2022-09-01 20:00:47');
