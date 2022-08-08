aac5b478b2b44b26561bdc1eda003059035fe474
Panov Alexey <alexeykapanov@gmail.com>
2022-08-08 19:32:48 +0300

    Implement getting records by id and getting record collections by using client app.

--- client/internal/handlers/cli/cli.go
+++ client/internal/handlers/cli/cli.go
@@ -9,6 +9,8 @@ import (
 	"github.com/manifoldco/promptui"
 	"log"
 	"os"
+	"strings"
+	"text/tabwriter"
 )
 
 type ConsoleCommandHandler struct {
@@ -60,14 +62,29 @@ func (h ConsoleCommandHandler) auth() {
 	default:
 		h.auth()
 	}
+}
 
-	fmt.Printf("You choose %q\n", result)
+func (h ConsoleCommandHandler) getItemType() string {
+	prompt := promptui.Select{
+		Label: "Choose type for getting detail info",
+		Items: []string{"login password", "credit card", "text record", "binary record", "quit"},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+
+	return result
 }
 
 func (h ConsoleCommandHandler) register() {
 	fmt.Printf("Enter your email:")
 	reader := bufio.NewReader(os.Stdin)
 	email, err := reader.ReadString('\n')
+	email = strings.TrimSuffix(email, "\n")
 
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
@@ -77,6 +94,7 @@ func (h ConsoleCommandHandler) register() {
 	fmt.Printf("Enter your password:")
 	reader = bufio.NewReader(os.Stdin)
 	password, err := reader.ReadString('\n')
+	password = strings.TrimSuffix(password, "\n")
 
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
@@ -86,6 +104,7 @@ func (h ConsoleCommandHandler) register() {
 	fmt.Printf("Repeat your password:")
 	reader = bufio.NewReader(os.Stdin)
 	repeatPassword, err := reader.ReadString('\n')
+	repeatPassword = strings.TrimSuffix(repeatPassword, "\n")
 
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
@@ -99,10 +118,6 @@ func (h ConsoleCommandHandler) register() {
 			RepeatPassword: repeatPassword,
 		},
 	)
-
-	//////////////////
-	log.Println(token) //ToDo: delete it
-	log.Println(err)   //ToDo: delete it
 
 	if err != nil || token == "" {
 		fmt.Print("registration error. ")
@@ -122,6 +137,7 @@ func (h ConsoleCommandHandler) login() {
 	fmt.Printf("Enter your email:")
 	reader := bufio.NewReader(os.Stdin)
 	email, err := reader.ReadString('\n')
+	email = strings.TrimSuffix(email, "\n")
 
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
@@ -131,6 +147,7 @@ func (h ConsoleCommandHandler) login() {
 	fmt.Printf("Enter your password:")
 	reader = bufio.NewReader(os.Stdin)
 	password, err := reader.ReadString('\n')
+	password = strings.TrimSuffix(password, "\n")
 
 	if err != nil {
 		fmt.Println("An error occured while reading input. Please try again", err)
@@ -182,28 +199,307 @@ func (h ConsoleCommandHandler) enterMainCommand() {
 
 	switch result {
 	case "get-all-data":
-		userData, err := h.userDataService.GetAllData()
-		fmt.Println(userData)
-		fmt.Println(err)
+		userData, err := h.userDataService.GetAllData(h.config.GetToken())
+
+		if err != nil {
+			fmt.Println(err) //@ToDo: check it
+		}
+
+		h.showList("Login passwords", userData.LoginPasswordCollection)
+		h.showList("Credit cards", userData.CreditCardCollection)
+		h.showList("Binary records", userData.BinaryRecordCollection)
+		h.showList("Text records", userData.TextRecordCollection)
+
+		itemType := h.getItemType()
+
+		switch itemType {
+		case "login password":
+			h.getLoginPassword(userData.LoginPasswordCollection)
+		case "credit card":
+			h.getCreditCard(userData.CreditCardCollection)
+		case "binary record":
+			h.getBinaryRecord(userData.BinaryRecordCollection)
+		case "text record":
+			h.getBinaryRecord(userData.TextRecordCollection)
+		case "quit":
+			h.quit()
+		default:
+			h.RunDialog()
+		}
+
+		h.RunDialog()
 	case "get-login-password":
-		loginPasswordCollection, err := h.userDataService.GetLoginPasswordCollection()
-		fmt.Println(loginPasswordCollection)
-		fmt.Println(err)
+		loginPasswordCollection, err := h.userDataService.GetLoginPasswordCollection(h.config.GetToken())
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		h.showList("Login passwords", loginPasswordCollection)
+		h.getLoginPassword(loginPasswordCollection)
 	case "get-credit-card":
-		creditCardCollection, err := h.userDataService.GetCreditCardCollection()
-		fmt.Println(creditCardCollection)
-		fmt.Println(err)
+		creditCardCollection, err := h.userDataService.GetCreditCardCollection(h.config.GetToken())
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		h.showList("Credit cards", creditCardCollection)
+		h.getCreditCard(creditCardCollection)
 	case "get-text-record":
-		textRecordCollection, err := h.userDataService.GetTextRecordCollection()
-		fmt.Println(textRecordCollection)
-		fmt.Println(err)
+		textRecordCollection, err := h.userDataService.GetTextRecordCollection(h.config.GetToken())
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		h.showList("Text records", textRecordCollection)
+		h.getTextRecord(textRecordCollection)
 	case "get-binary-record":
-		binaryRecordCollection, err := h.userDataService.GetBinaryRecordCollection()
-		fmt.Println(binaryRecordCollection)
-		fmt.Println(err)
+		binaryRecordCollection, err := h.userDataService.GetBinaryRecordCollection(h.config.GetToken())
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		h.showList("Binary records", binaryRecordCollection)
+		h.getBinaryRecord(binaryRecordCollection)
 	case "quit":
 		h.quit()
 	default:
 		h.enterMainCommand()
 	}
+
+	h.RunDialog()
+}
+
+func (h ConsoleCommandHandler) getLoginPassword(collection []domain.ProtectedItem) {
+	itemId := h.getItemIdByCollection(collection)
+
+	if itemId != "" {
+		loginPassword, err := h.getLoginPasswordById(itemId)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		h.showLoginPassword(*loginPassword)
+	}
+}
+
+func (h ConsoleCommandHandler) getCreditCard(collection []domain.ProtectedItem) {
+	itemId := h.getItemIdByCollection(collection)
+
+	if itemId != "" {
+		creditCard, err := h.getCreditCardById(itemId)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		h.showCreditCard(*creditCard)
+	}
+}
+
+func (h ConsoleCommandHandler) getTextRecord(collection []domain.ProtectedItem) {
+	itemId := h.getItemIdByCollection(collection)
+
+	if itemId != "" {
+		text, err := h.getTextById(itemId)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		h.showTextRecord(*text)
+	}
+}
+
+func (h ConsoleCommandHandler) getBinaryRecord(collection []domain.ProtectedItem) {
+	itemId := h.getItemIdByCollection(collection)
+
+	if itemId != "" {
+		binary, err := h.getBinaryById(itemId)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		h.showBinaryRecord(*binary)
+	}
+}
+
+func (h ConsoleCommandHandler) getLoginPasswordById(id string) (*domain.LoginPassword, error) {
+	item, err := h.userDataService.GetLoginPasswordById(h.config.GetToken(), id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return item, err
+}
+
+func (h ConsoleCommandHandler) getCreditCardById(id string) (*domain.CreditCard, error) {
+	item, err := h.userDataService.GetCreditCardById(h.config.GetToken(), id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return item, err
+}
+
+func (h ConsoleCommandHandler) getBinaryById(id string) (*domain.BinaryRecord, error) {
+	item, err := h.userDataService.GetBinaryRecordById(h.config.GetToken(), id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return item, err
+}
+
+func (h ConsoleCommandHandler) getTextById(id string) (*domain.TextRecord, error) {
+	item, err := h.userDataService.GetTextRecordById(h.config.GetToken(), id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return item, err
+}
+
+func (h ConsoleCommandHandler) getItemIdByCollection(collections []domain.ProtectedItem) string {
+	var idCollection []string
+
+	for _, item := range collections {
+		idCollection = append(idCollection, item.Id)
+	}
+
+	prompt := promptui.Select{
+		Label: "Choose item id for getting detail info",
+		Items: idCollection,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+
+	return result
+}
+
+func (h ConsoleCommandHandler) showList(name string, list []domain.ProtectedItem) error {
+	fmt.Println("####################################")
+	fmt.Println(name)
+	fmt.Println("####################################")
+	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "id\tname\tcreated_at\tlast_access_at\t")
+
+	if len(list) == 0 {
+		fmt.Fprintln(w, "no data...\t")
+	}
+
+	for _, item := range list {
+		fmt.Fprintln(w, item.Id+"\t"+item.Name+"\t"+item.CreatedAt+"\t"+item.LastAccessAt)
+	}
+
+	err := w.Flush()
+
+	return err
+}
+
+func (h ConsoleCommandHandler) showLoginPassword(item domain.LoginPassword) error {
+	fmt.Println("####################################")
+	fmt.Println("Detail info about login password with id=" + item.Id)
+	fmt.Println("####################################")
+	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "id\tname\tlogin\tpassword\tnote\tcreated_at\tlast_access_at\t")
+	fmt.Fprintln(
+		w,
+		item.Id+"\t"+item.Name+"\t"+item.Login+"\t"+item.Password+
+			"\t"+item.Note+"\t"+item.CreatedAt+"\t"+item.LastAccessAt,
+	)
+
+	err := w.Flush()
+
+	return err
+}
+
+func (h ConsoleCommandHandler) showCreditCard(item domain.CreditCard) error {
+	fmt.Println("####################################")
+	fmt.Println("Detail info about credit card with id=" + item.Id)
+	fmt.Println("####################################")
+	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "id\tname\tnumber\texp\tcvv\towner\tnote\tcreated_at\tlast_access_at\t")
+	fmt.Fprintln(
+		w,
+		item.Id+"\t"+
+			item.Name+"\t"+
+			item.Number+"\t"+
+			item.Expiration+"\t"+
+			item.Cvv+"\t"+
+			item.Owner+"\t"+
+			item.Note+"\t"+
+			item.CreatedAt+"\t"+
+			item.LastAccessAt,
+	)
+
+	err := w.Flush()
+
+	return err
+}
+
+func (h ConsoleCommandHandler) showTextRecord(item domain.TextRecord) error {
+	fmt.Println("####################################")
+	fmt.Println("Detail info about text record with id=" + item.Id)
+	fmt.Println("####################################")
+
+	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "id\tname\ttext\tnote\tcreated_at\tlast_access_at\t")
+	fmt.Fprintln(
+		w,
+		item.Id+"\t"+
+			item.Name+"\t"+
+			item.Text+"\t"+
+			item.Note+"\t"+
+			item.CreatedAt+"\t"+
+			item.LastAccessAt,
+	)
+
+	err := w.Flush()
+
+	return err
+}
+
+func (h ConsoleCommandHandler) showBinaryRecord(item domain.BinaryRecord) error {
+	fmt.Println("####################################")
+	fmt.Println("Detail info about binary record with id=" + item.Id)
+	fmt.Println("####################################")
+
+	w := tabwriter.NewWriter(os.Stdout, 5, 1, 1, ' ', 0)
+	fmt.Fprintln(w, "id\tname\tbinary\tnote\tcreated_at\tlast_access_at\t")
+	fmt.Fprintln(
+		w,
+		item.Id+"\t"+
+			item.Name+"\t"+
+			item.Binary+"\t"+
+			item.Note+"\t"+
+			item.CreatedAt+"\t"+
+			item.LastAccessAt,
+	)
+
+	err := w.Flush()
+
+	return err
 }
---
+++ client/internal/infrastructure/http/apiClient.go
@@ -0,0 +1,320 @@
+package http
+
+import (
+	"bytes"
+	"client/internal/domain"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type ApiClient struct {
+	serverAddress string
+	client        http.Client
+}
+
+func GetApiClient(serverAddress string, maxIdleConnections, httpTimeout int) ApiClient {
+	return ApiClient{
+		serverAddress: serverAddress,
+		client:        getHttpClient(maxIdleConnections, httpTimeout),
+	}
+}
+
+func getHttpClient(maxIdleConnections, httpTimeout int) http.Client {
+	client := http.Client{}
+	transport := &http.Transport{}
+	transport.MaxIdleConns = maxIdleConnections
+	client.Transport = transport
+	client.Timeout = time.Second * time.Duration(httpTimeout)
+
+	return client
+}
+
+func (c ApiClient) getRegistrationRequest(user domain.User) *http.Request {
+	body, err := json.Marshal(user)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, c.serverAddress+"/api/v1/signup", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+
+	return request
+}
+
+func (c ApiClient) getAuthRequest(user domain.User) *http.Request {
+	body, err := json.Marshal(user)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, c.serverAddress+"/api/v1/auth", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+
+	return request
+}
+
+func (c ApiClient) getAllDataRequest(token string) *http.Request {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		c.serverAddress+"/api/v1/data/all",
+		nil,
+	)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getLoginPasswordRequest(token string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/login-password", nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getTextRecordRequest(token string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/text-record", nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getBinaryRecordRequest(token string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/binary-record", nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreditCardRequest(token string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/credit-card", nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreateLoginPasswordRequest(token string) *http.Request {
+	loginPassword := domain.LoginPassword{
+		Name:     "Тут будет имя",
+		Login:    "Тут будет логин",
+		Password: "qwertypass",
+		Note:     "Запись с клиента, ура!",
+	}
+	body, err := json.Marshal(loginPassword)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request, err := http.NewRequest(
+		http.MethodPost,
+		c.serverAddress+"/api/v1/data/login-password",
+		bytes.NewBuffer(body),
+	)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreateTextRecordRequest(token string) *http.Request {
+	textRecord := domain.TextRecord{
+		Name: "laboris officia nostrud exercitation",
+		Text: "velit amet id",
+		Note: "Excepteur",
+	}
+	body, err := json.Marshal(textRecord)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request, err := http.NewRequest(
+		http.MethodPost,
+		c.serverAddress+"/api/v1/data/text-record",
+		bytes.NewBuffer(body),
+	)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreateBinaryRecordRequest(token string) *http.Request {
+	binaryRecord := domain.BinaryRecord{
+		Name:   "laboris officia nostrud exercitation",
+		Binary: "0101010101010101",
+		Note:   "Excepteur",
+	}
+	body, err := json.Marshal(binaryRecord)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request, err := http.NewRequest(
+		http.MethodPost,
+		c.serverAddress+"/api/v1/data/binary-record",
+		bytes.NewBuffer(body),
+	)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreateCreditCardRequest(token string) *http.Request {
+	creditCard := domain.CreditCard{
+		Name:       "et magna",
+		Number:     "cupidatat laboris ullamco",
+		Expiration: "e3",
+		Cvv:        "c33",
+		Owner:      "dquat",
+		Note:       "fugiat",
+	}
+	body, err := json.Marshal(creditCard)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, c.serverAddress+"/api/v1/data/credit-card", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getLoginPasswordByIdRequest(token, id string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/login-password/"+id, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getTextRecordByIdRequest(token, id string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/text-record/"+id, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getBinaryRecordByIdRequest(token, id string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/binary-record/"+id, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getCreditCardByIdRequest(token, id string) *http.Request {
+	request, err := http.NewRequest(http.MethodGet, c.serverAddress+"/api/v1/data/credit-card/"+id, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Token", token)
+
+	return request
+}
+
+func (c ApiClient) getContentFromResponse(response *http.Response) (*string, error) {
+	content := ""
+	err := json.NewDecoder(response.Body).Decode(&content)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &content, nil
+}
